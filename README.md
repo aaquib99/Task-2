5fe7be7d4a7253b794490534d4d39e554c5183d5
aaquib99 <90325708+aaquib99@users.noreply.github.com>
2022-11-11 15:50:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-In this folder main file in app.py (conatain python code) .
-inputFile.txt is a text file for which we are checking the count of nouns, adjectives and verbs .
-After getting the result it will get stored in result.txt .
-logFile basically used as a log file of issues and errors . 
+Objective : Write a program to count the number of nouns, adjectives and verbs in a given text file. You can use any existing POS taggers to do the task. Results should be stored in another file. A log file of issues and errors should also be created. Use Python.
+
+In this folder main file in app.py (conatain python code).
+inputFile.txt is a text file for which we are checking the count of nouns, adjectives and verbs.
+After getting the result it will get stored in result.txt.
+logFile basically used as a log file of issues and errors. 
--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import nltk
 nltk.download('punkt')
 from collections import Counter
+from datetime import datetime
 try:
     print("Hello")
     f = open("inputFile.txt","r", encoding="utf8")
     str = f.read()
+    # print(str)
+    #Data get stored in str variable from input file.
     text = nltk.word_tokenize(str)
+    # print(text)
+    #Created the list of all words to identify.
     ans = nltk.pos_tag(text)
     # print(ans)
+    #Here with the help of pos tags eah word will get its tag
     counts = Counter(tag for word,tag in ans)
+    #Here  we will count the which tag has how many values.
     # print("Counts",counts)
     res  = dict((word,count) for word,count in counts.items())
+    #Just to remove tuple from outside of of counts and stored that in res
+    # print(res)
     for i in res :
         if(i == "JJ"):
             adjective = res[i]
@@ -18,15 +27,21 @@ try:
             noun = res[i]
         elif(i == "VB"):
             verb = res[i]
-
+        elif(i == "NNP"):
+            noun = noun + res[i]
     # print(noun,adjective,verb)
-    result = f'Number of Nouns is given file is {noun} ,verb count is {verb} and adjective count is {adjective}'
+    #Counted all the requires details
+    result = f'Number of Nouns is {noun} ,verb count is {verb} and adjective count is {adjective}'
     file = open("result.txt", "w")
+    #result will be stored in result.txt
     file.write(result)
 
     
-except Exception as e:
-    print(e)
-    error = f'Error in file app.py {e}\n'
+except Exception as error:
+    #Here all type of exception will be handled and stored in error variable
+    print(error)
+    current_time = datetime.now()
+    error = f'{current_time} : Error in file app.py {error}\n'
     file = open("logFile.txt", "a+")
+    #here this error will be stored in logFile.txt
     file.write(error)
